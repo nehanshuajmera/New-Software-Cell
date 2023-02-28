5f5029f85e191e861549c27d6fcfdb1312f7a76d
sharma39vishal <sharma39vishal@gmail.com>
2023-02-28 20:35:31 +0530

    .

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import About from './About'
 import HeroSection from './HeroSection'
 
 export default function Home() {
   return (
     <div>
-      <HeroSection/>
+      {/* <HeroSection/> */}
+      <About/>
     </div>
   )
 }
