fa305fd7bb946f9d2ba69536f268c5d4c486a51d
sharma39vishal <sharma39vishal@gmail.com>
2023-03-02 10:53:46 +0530

    d

--- client/src/Pages/Home/About.jsx
+++ client/src/Pages/Home/About.jsx
@@ -5,9 +5,11 @@ export default function About() {
   return (
     <div>
         <div  className='about-section'>
+          <div className='right-box'>
+          <h1 style={{textAlign:"center"}}>About Us</h1>          
        <AboutCard/>
-        <div className='left-section'>
-        <h1 style={{textAlign:"center"}}>About Us</h1>          
+       </div>
+        <div className='left-box'>
             <h3 style={{textAlign:"center"}}> 
         Medi-Caps University has established a Software Cell with the objective of enhancing the skills and knowledge of its students in the field of software development.
 The Software Cell at Medi-Caps University is committed to equipping students with the latest tools and techniques in software development. Its primary aim is to bridge the gap between industry requirements and academic curriculums by organising various training sessions and providing internships. Additionally, the Software Cell offers students opportunities to work on real-world projects, thereby enabling them to gain practical experience in the software development field.
--- client/src/Pages/Home/MainSection.jsx
+++ client/src/Pages/Home/MainSection.jsx
@@ -5,17 +5,16 @@ import "./mainsection.css"
 export default function MainSection() {
     const navigate=useNavigate();
   return (
-    <div className='mainsection row'>
-      
-        <div className='column'>
-            <h1>MediCaps Software Cell</h1>
+    <div className='mainsection'>
+        <div className='right-box'>
+            <h1 >MediCaps Software Cell</h1>
             <h2>Let Connect, Develope, Code !</h2>
             <button onClick={()=>{navigate("/joinus")}} data-label="Register" class="rainbow-hover">
             <span class="sp">Join Us</span>
             </button>
         </div>
-        <div className='column'>
-            <img src='./images/coding.png' width={"80%"} alt=''/>
+        <div className='left-box'>
+            <img src='./images/coding.png' width={"360px"} alt=''/>
         </div>
         
     </div>
